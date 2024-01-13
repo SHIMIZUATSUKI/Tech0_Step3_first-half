f5ccee643f4200f71db007ebd492e867bf794741
SHIMIZUATSUKI <atklikmngkr.33@gmail.com>
2024-01-13 22:20:54 +0900

    app6.pyの修正とrequirements.txtの修正

--- Step3_first-half/2024.0118/app6.py
+++ Step3_first-half/2024.0118/app6.py
@@ -2,17 +2,14 @@ import streamlit as st
 import pandas as pd
 from sqlalchemy import create_engine
 
-@st.cache(ttl=3600, max_entries=10, allow_output_mutation=True)
-def load_data():
-    # st.secretsを使用してデータベースURLを取得
-    database_url = st.secrets["database"]["url"]
-    
-    # 取得したURLを使用してSQLAlchemyのエンジンを作成し、データベースに接続
-    engine = create_engine(database_url)
+# データベースエンジンをグローバルスコープで作成
+database_url = "sqlite:///Tokyo_RealEstate_DB.db"  # 本来は st.secrets["database"]["url"] を使用
+engine = create_engine(database_url)
 
-    # SQLクエリを実行し、結果をDataFrameに読み込む
-    query = 'SELECT * FROM "Ota_Ward"'
-    df = pd.read_sql(query, engine)
+def load_data():
+    with engine.connect() as connection:
+        query = 'SELECT * FROM "Ota_Ward"'
+        df = pd.read_sql(query, connection)
     
     # 英語のカラム名を日本語に変更する変換
     column_translations_reversed = {
@@ -46,7 +43,6 @@ def load_data():
     
     return df
 
-
 def filter_data(df, time_range, layout, rent_range, age_range):
     # 最寄駅からの時間でフィルタリング
     df = df[df['最寄駅1からの時間(分)'].between(time_range[0], time_range[1])]
@@ -78,6 +74,7 @@ def main():
         st.dataframe(filtered_df[['物件名', 'アクセス', '間取り', '家賃(円)', '管理費(円)', '敷金(円)', '礼金(円)','建物の階数','面積(m2)','築年数(年)', '住所']], width=1200, height=500)
     else:
         st.write("←左の入力欄から条件を設定し、'検索'ボタンを押してください。")
+
 
 if __name__ == '__main__':
     main()
